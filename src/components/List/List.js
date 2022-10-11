07f483081e2122524321f19c711a8ab1790acbdc
Kenneth <iamknf@gmail.com>
2022-10-10 19:46:45 -1000

    built out PlaceDetails and List components, added useEffect for geolocation to get current position and implemented an api call with the bounds.sw/ne arguments

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   CircularProgress,
   Grid,
@@ -8,13 +8,13 @@ import {
   FormControl,
   Select,
 } from "@material-ui/core";
-
+import PlaceDetails from '../PlaceDetails/PlaceDetails'
 import useStyles from "./styles";
 
-const List = () => {
+const List = ({places}) => {
   const classes = useStyles();
-  const [type, setType] = useState('restaurants')
-  const [rating, setRating] = useState('restaurants')
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
 
   return (
     <div className={classes.container}>
@@ -23,15 +23,28 @@ const List = () => {
       </Typography>
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
-        <Select
-        value={type}
-        onChange={(e)=>setType(e.target.value)}
-        >
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
           <MenuItem value="restaurants">Restaurants</MenuItem>
           <MenuItem value="hotels">Hotels</MenuItem>
           <MenuItem value="attractions">Attractions</MenuItem>
         </Select>
       </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3</MenuItem>
+          <MenuItem value={4}>Above 4</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place}/>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
